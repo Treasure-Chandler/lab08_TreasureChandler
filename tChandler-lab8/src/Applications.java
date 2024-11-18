fc3610eee4847d9207b1a962f3627776ca0b57fd
૮꒰ ˶• ༝ •˶꒱ა ♡ <treasure89238@students.sbcsc.k12.in.us>
2024-11-18 12:10:03 -0500

    Started lab08

---
+++ tChandler-lab8/src/Applications.java
@@ -0,0 +1,17 @@
+/**
+ * @author Treasure Chandler
+ * CS 16000-01 02/03, Fall Semester 2024
+ * Lab 8
+ * 
+ * 
+ */
+
+public class Applications {
+    /**
+     * 
+     * @param args      Entered values
+     */
+    public static void main(String[] args) {
+
+    } // End of main()
+} // End of Applications
---
+++ tChandler-lab8/src/Rectangle.java
@@ -0,0 +1,11 @@
+/**
+ * @author Treasure Chandler
+ * CS 16000-01 02/03, Fall Semester 2024
+ * Lab 8
+ * 
+ * 
+ */
+
+public class Rectangle {
+
+} // End of Rectangle
